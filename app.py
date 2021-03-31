4421cc5e403615aaea1b672e7bab4953b210c2eb
Deep santani <dipsantani789@gmail.com>
2021-03-31 15:07:03 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Mar 30 16:16:20 2021
+
+@author: Deep0p & Meet0p
+"""
+
+from flask import Flask, render_template, request
+#from googleapiclient.discovery import build
+from apiclient.discovery import build
+
+api_key ="AIzaSyCf2Yoff3AWC09fQFS7dfSgcVcUTPUVKWk"
+yotube = build('youtube','v3',developerKey=api_key)
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+	return render_template('index.html')
+
+@app.route('/search', methods=['POST'])
+def predict():
+    if request.method == 'POST':
+        search = (request.form['Search'])
+        
+        req = yotube.search().list(q=search,part='snippet',type='video')
+        res = req.execute()
+        
+        listt=[]
+        for item in res['items']:
+            id=item['id']['videoId']
+            youtube="https://youtube.com/embed/"
+            id=youtube+id
+            listt.append(id)        
+        
+        
+        return render_template('index.html')
+    
+if __name__ == '__main__':
+	app.run(debug=True)    
+
+    
